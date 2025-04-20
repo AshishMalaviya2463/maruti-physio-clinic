0f3bbd2b55264b206eda57972386eec4e6e9a88d
Ashish malaviya <ashishmalaviya2424@gmail.com>
2025-04-20 10:19:11 +0530

    initialze repo

---
+++ app/page.js
@@ -0,0 +1,15 @@
+import { Typography } from "@mui/material";
+
+export default function Home() {
+  return (
+    <>
+      <Typography
+        sx={{
+          color: "red",
+        }}
+      >
+        Hello World
+      </Typography>
+    </>
+  );
+}
